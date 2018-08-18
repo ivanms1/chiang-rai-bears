4c9622e0c252339b019998420e148f3be92d5972
Ivan S <ivanms1@gmail.com>
2018-08-18 22:33:50 +0700

    fix a element

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
 	<div className="footer">
 		<div className="footer-content">
 			<p>© Chiang Rai Bears 2018. All Rights Reserved</p>
-			<p className="credits">Made by <a href="https://www.ivansaldano.com" target="_blank">Ivan</a></p>
+			<p className="credits">Made by <a href="https://www.ivansaldano.com" rel="noopener noreferrer" target="_blank">Ivan</a></p>
 		</div>		
 	</div>
 	)
